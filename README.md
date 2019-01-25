41fa35c36a6e010f58a7432832f603d1230901a6
Andrew Davis <atd285@gmail.com>
2019-01-25 17:36:28 -0500

    operators working

--- README.md
+++ README.md
@@ -1,29 +1,26 @@
 # calculator-app
 
-## Project setup
-```
-npm install
-```
+## Steps
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+- [x] Create App.vue
+- [x] Create basic layout
+- [ ] Store buttons on data
+  - text
+  - type
+- [x] Style it!
+  - [x] Button Spacing
+  - [x] Button Colors
+  - [x] Button Hover
+  - [x] Responsive
+    - [x] Size based on screen size
+    - [x] Centered
+- [ ] Make it work!
+  - [x] Number button appends displayed number to screen
+  - [x] AC button clears the display
+  - [x] +/- button inverts the display value and updates the display
+  - [x] Operation button (+ - ÷ ×) stores displayed number/sets operation
+  - [x] Number button pressed after operation, update display with new number
+  - [x] Equals button performs operation and updates display
+  - [ ] Period sets decimal, cannot add more than 1 period
+  - [ ] Should not allow zero at beginning of number
+- [ ] _STRETCH_ Display text grows/shrinks to length of display
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,170 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="html">
+  <main class="calculator">
+    <div ref="display" class="display">
+      <div class="display-text">
+        <span ref="displayText">{{display}}</span>
+      </div>
+    </div>
+    <div class="buttons">
+      <div class="button-row" v-for="row in buttonRows">
+        <div
+          :class="{ operator: button.type == 'operator' }"
+          :style="button.style"
+          class="button"
+          v-for="button in row"
+          v-on:click="buttonClick(button)"
+
+          >
+          {{button.text}}
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  methods: {
+    buttonClick(button) {
+      if (button.type === 'number') {
+        if (this.previousValue === null) {
+          this.previousValue = Number(this.display);
+          this.display = '';
+        }
+        this.display += button.text;
+      } else if (button.text === 'AC') {
+        this.display = '';
+      } else if (button.text === '.') {
+        if (this.display.includes('.') || this.display === '') return;
+        else {
+          this.display = this.display + button.text;
+        }
+      } else if (button.text === '+/-') {
+        this.display *= -1;
+      } else if (button.type === 'operator') {
+        if (this.currentOperator) {
+          const operationType = this.operations[this.currentOperator];
+          this.display = operationType(
+            this.previousValue,
+            parseInt(this.display)
+          );
+        }
+        this.previousValue = null;
+        this.currentOperator = button.text;
+      } else if (button.text === '=') {
+        const evaluator = this.operations[this.currentOperator];
+        this.currentOperator = '';
+        this.display = evaluator(+this.previousValue, +this.display);
+      }
+    },
+  },
+  data: () => ({
+    currentOperator: '',
+    previousValue: null,
+    total: null,
+    operations: {
+      '÷': (a, b) => (a % b !== 0 ? (a / b).toFixed(1) : a / b),
+      '×': (a, b) => a * b,
+      '-': (a, b) => a - b,
+      '+': (a, b) => a + b,
+    },
+    display: '',
+    buttonRows: [
+      [
+        { text: 'AC', type: 'special' },
+        { text: '+/-', type: 'special' },
+        { text: '😊', type: 'special' },
+        { text: '÷', type: 'operator' },
+      ],
+      [
+        { text: '7', type: 'number' },
+        { text: '8', type: 'number' },
+        { text: '9', type: 'number' },
+        { text: '×', type: 'operator' },
+      ],
+      [
+        { text: '4', type: 'number' },
+        { text: '5', type: 'number' },
+        { text: '6', type: 'number' },
+        { text: '-', type: 'operator' },
+      ],
+      [
+        { text: '1', type: 'number' },
+        { text: '2', type: 'number' },
+        { text: '3', type: 'number' },
+        { text: '+', type: 'operator' },
+      ],
+      [
+        { text: '0', type: 'number', style: 'flex-basis: calc(100%/2)' },
+        { text: '.', type: 'special', style: 'flex-basis: calc(100%/2)' },
+        { text: '=', type: 'special' },
+      ],
+    ],
+  }),
+  components: {},
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  width: 100vw;
+  height: 100vh;
+  font-size: 2.5vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.calculator {
+  width: 30vw;
+  height: calc(30vw * 1.4);
+  font-family: sans-serif;
+  border-radius: 5px;
+  overflow: hidden;
+  border: 0.5px solid grey;
+}
+.display {
+  height: calc(30vw * 0.2);
+  background: #989898;
+  color: white;
+  font-size: 4vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+.display-text {
+  padding: 0.5vw;
+  text-align: right;
+}
+.button-row {
+  display: flex;
+  height: 20%;
+  width: 100%;
+  justify-content: space-around;
+}
+.buttons {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: calc(30vw * 1.2);
+}
+.button {
+  display: inline-block;
+  outline: 0.5px solid grey;
+  width: 100%;
+  background: #d0d0d0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.operator {
+  background: #f6872c;
+  color: #fff;
+}
+.button:hover {
+  background: hsl(220, 24%, 93%);
+  cursor: pointer;
+}
+.button:active {
+  background: #b5b4b3;
 }
 </style>
