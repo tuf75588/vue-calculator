22ff2bf7002bfbaf5abf1314ffc2f8ce34bb7a7d
Andrew Davis <atd285@gmail.com>
2019-01-25 20:39:07 -0500

    finish app add github pages

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,11 @@
     <div class="buttons">
       <div class="button-row" v-for="row in buttonRows">
         <div
+          @click="buttonClicks[button.type](button)"
           :class="{ operator: button.type == 'operator' }"
           :style="button.style"
           class="button"
-          v-for="button in row"
-          v-on:click="buttonClick(button)"
-
-          >
+          v-for="button in row">
           {{button.text}}
         </div>
       </div>
@@ -24,89 +22,172 @@
 
 <script>
 export default {
-  name: 'app',
   methods: {
-    buttonClick(button) {
-      if (button.type === 'number') {
-        if (this.previousValue === null) {
-          this.previousValue = Number(this.display);
-          this.display = '';
-        }
-        this.display += button.text;
-      } else if (button.text === 'AC') {
-        this.display = '';
-      } else if (button.text === '.') {
-        if (this.display.includes('.') || this.display === '') return;
-        else {
-          this.display = this.display + button.text;
-        }
-      } else if (button.text === '+/-') {
-        this.display *= -1;
-      } else if (button.type === 'operator') {
-        if (this.currentOperator) {
-          const operationType = this.operations[this.currentOperator];
-          this.display = operationType(
-            this.previousValue,
-            parseInt(this.display)
+    updateDisplay(value) {
+      this.display = value;
+      this.$nextTick(() => {
+        let fontSize = 4;
+        this.$refs.display.setAttribute('style', 'font-size:4vw');
+        while (
+          this.$refs.displayText.offsetWidth + 30 >
+          this.$refs.display.offsetWidth
+        ) {
+          this.$refs.display.setAttribute(
+            'style',
+            'font-size:' + fontSize + 'vw'
           );
+          fontSize -= 0.1;
+          if (fontSize <= 0.1) break;
         }
-        this.previousValue = null;
-        this.currentOperator = button.text;
-      } else if (button.text === '=') {
-        const evaluator = this.operations[this.currentOperator];
-        this.currentOperator = '';
-        this.display = evaluator(+this.previousValue, +this.display);
+      });
+    },
+    performOperation() {
+      if (this.previousValue) {
+        const value = this.operations[this.currentOperator](
+          +this.previousValue,
+          +this.display
+        );
+        this.updateDisplay(value);
       }
     },
   },
-  data: () => ({
+  data: (vm) => ({
+    display: '0',
+    previousValue: '',
     currentOperator: '',
-    previousValue: null,
-    total: null,
+    buttonClicks: {
+      number(button) {
+        if (vm.previousValue === null) {
+          vm.previousValue = Number(vm.display);
+          vm.updateDisplay('');
+        }
+        if (button.text == '.' && vm.display.includes('.')) return;
+        vm.updateDisplay(vm.display + button.text);
+        if (
+          vm.display.length > 1 &&
+          vm.display[0] == '0' &&
+          vm.display[1] != '.'
+        ) {
+          vm.updateDisplay(vm.display.slice(1));
+        }
+      },
+      operator(button) {
+        if (vm.currentOperator) {
+          vm.performOperation();
+        }
+        vm.previousValue = null;
+        vm.currentOperator = button.text;
+      },
+      special(button) {
+        if (button.text == 'AC') {
+          vm.updateDisplay('0');
+        } else if (button.text == '+/-') {
+          vm.updateDisplay(vm.display * -1);
+        } else if (button.text == '=') {
+          vm.performOperation();
+          vm.currentOperator = '';
+        }
+      },
+    },
     operations: {
-      '÷': (a, b) => (a % b !== 0 ? (a / b).toFixed(1) : a / b),
+      '÷': (a, b) => a / b,
       '×': (a, b) => a * b,
       '-': (a, b) => a - b,
       '+': (a, b) => a + b,
     },
-    display: '',
     buttonRows: [
       [
-        { text: 'AC', type: 'special' },
-        { text: '+/-', type: 'special' },
-        { text: '😊', type: 'special' },
-        { text: '÷', type: 'operator' },
+        {
+          text: 'AC',
+          type: 'special',
+        },
+        {
+          text: '+/-',
+          type: 'special',
+          style: 'flex-basis: calc(100%/2)',
+        },
+        {
+          text: '÷',
+          type: 'operator',
+          style: 'flex-basis: calc(100%/2)',
+        },
       ],
       [
-        { text: '7', type: 'number' },
-        { text: '8', type: 'number' },
-        { text: '9', type: 'number' },
-        { text: '×', type: 'operator' },
+        {
+          text: '7',
+          type: 'number',
+        },
+        {
+          text: '8',
+          type: 'number',
+        },
+        {
+          text: '9',
+          type: 'number',
+        },
+        {
+          text: '×',
+          type: 'operator',
+        },
       ],
       [
-        { text: '4', type: 'number' },
-        { text: '5', type: 'number' },
-        { text: '6', type: 'number' },
-        { text: '-', type: 'operator' },
+        {
+          text: '4',
+          type: 'number',
+        },
+        {
+          text: '5',
+          type: 'number',
+        },
+        {
+          text: '6',
+          type: 'number',
+        },
+        {
+          text: '-',
+          type: 'operator',
+        },
       ],
       [
-        { text: '1', type: 'number' },
-        { text: '2', type: 'number' },
-        { text: '3', type: 'number' },
-        { text: '+', type: 'operator' },
+        {
+          text: '1',
+          type: 'number',
+        },
+        {
+          text: '2',
+          type: 'number',
+        },
+        {
+          text: '3',
+          type: 'number',
+        },
+        {
+          text: '+',
+          type: 'operator',
+        },
       ],
       [
-        { text: '0', type: 'number', style: 'flex-basis: calc(100%/2)' },
-        { text: '.', type: 'special', style: 'flex-basis: calc(100%/2)' },
-        { text: '=', type: 'special' },
+        {
+          text: '0',
+          type: 'number',
+        },
+        {
+          text: '.',
+          type: 'number',
+          style: 'flex-basis: calc(100%/2)',
+        },
+        {
+          text: '=',
+          type: 'special',
+          style: 'flex-basis: calc(100%/2)',
+        },
       ],
     ],
   }),
-  components: {},
 };
 </script>
 
-<style>
+<style lang="css">
 body {
   width: 100vw;
   height: 100vh;
@@ -115,7 +196,6 @@ body {
   justify-content: center;
   align-items: center;
 }
-
 .calculator {
   width: 30vw;
   height: calc(30vw * 1.4);
@@ -137,15 +217,15 @@ body {
   padding: 0.5vw;
   text-align: right;
 }
+.buttons {
+  height: calc(30vw * 1.2);
+  text-align: center;
+}
 .button-row {
-  display: flex;
   height: 20%;
   width: 100%;
+  display: flex;
   justify-content: space-around;
-}
-.buttons {
-  text-align: center;
-  height: calc(30vw * 1.2);
 }
 .button {
   display: inline-block;
@@ -157,12 +237,8 @@ body {
   align-items: center;
 }
 .operator {
+  color: white;
   background: #f6872c;
-  color: #fff;
-}
-.button:hover {
-  background: hsl(220, 24%, 93%);
-  cursor: pointer;
 }
 .button:active {
   background: #b5b4b3;
